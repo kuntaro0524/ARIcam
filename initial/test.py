0100e3d01fb169136cd98f2a458bfc30f06a0a79
Kunio Hirata <kuntaro0524@gmail.com>
2025-08-26 10:52:04 +0900

    Important thing -> Install of neoapi to python3.8

--- initial/test.py
+++ initial/test.py
@@ -5,6 +5,7 @@ import cv2
 camera = neoapi.Cam()
 camera.Connect('ARI-Camera')
 camera.f.PixelFormat.SetString('RGB8')
+
 #camera.f.PixelFormat.SetString('Mono8')
 #設定値の取得と変更
 #ホワイトバランス調整可能なら実行
@@ -12,20 +13,22 @@ if(camera.HasFeature("BalanceWhiteAuto")) :
         print("BalanceWhiteAuto: %s" %camera.f.BalanceWhiteAuto.value)
         camera.f.BalanceWhiteAuto.SetString("Once")
 
+print("####################")
 print(camera.f.ExposureAutoMaxValue.value)
 print(camera.f.ExposureAutoMinValue.value)
+print("####################")
 
 #camera.f.ExposureTime.value = 30000.0
-camera.f.ExposureTime.Set(10000)
+# camera.f.ExposureTime.Set(1.0)
 camera.f.AcquisitionFrameRateEnable.value = True
 camera.f.AcquisitionFrameRate.value = 10
-print(camera.f.AcquisitionFrameRate.value)
+print(f"{camera.f.AcquisitionFrameRate.value} frame rate")
 
 while cv2.waitKey(1) != 27 :
         buffer = camera.GetImage()
 #        mat_buffer = buffer.GetNPArray().reshape(buffer.GetHeight(), buffer.GetWidth())
         mat_buffer = buffer.GetNPArray()
-        cv2.imshow("", mat_buffer)
+        cv2.imshow("TEST", mat_buffer)
 
 print(buffer.GetHeight(), buffer.GetWidth())
 cv2.imwrite("test.png",mat_buffer)
